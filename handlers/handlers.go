06096dfa05e5f83cc2f57d4cb842a1623f0901bf
winux <jpalbertini@live.fr>
2020-05-05 21:58:51 +0200

    module 3

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -56,5 +56,12 @@ func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 
 // WinnersHandler is the dispatcher for all /winners URL
 func WinnersHandler(res http.ResponseWriter, req *http.Request) {
-
+	switch req.Method {
+	case http.MethodPost:
+		AddNewWinner(res, req)
+	case http.MethodGet:
+		ListWinners(res, req)
+	default:
+		res.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
