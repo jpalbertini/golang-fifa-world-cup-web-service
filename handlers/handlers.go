5b692ec7ed947126d32d279bc02b29ba1d23e4ff
winux <jpalbertini@live.fr>
2020-05-05 21:31:50 +0200

    module 1

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,17 +1,39 @@
 package handlers
 
 import (
+	"golang-fifa-world-cup-web-service/data"
 	"net/http"
 )
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
-
+	res.WriteHeader(http.StatusNoContent)
 }
 
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
 
+	var winners []byte
+	var err error
+
+	year := req.URL.Query().Get("year")
+	if year == "" {
+		winners, err = data.ListAllJSON()
+	} else {
+		winners, err = data.ListAllByYear(year)
+	}
+
+	if err != nil {
+		if year != "" {
+			res.WriteHeader(http.StatusBadRequest)
+		} else {
+			res.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	res.Write(winners)
 }
 
 // AddNewWinner adds new winner to the list
